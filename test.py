14fd18570358a87552d566aa1a0660de296bdb03
Jaya <gujiakai28@gmail.com>
2023-10-22 13:14:24 +0000

    feat: add audio support

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os, sys
-
+import shutil
+from pydub import AudioSegment
 import gradio as gr
 import mdtex2html
 
@@ -20,9 +21,10 @@ from arguments import ModelArguments, DataTrainingArguments
 
 model = None
 tokenizer = None
+audio_text = ""
+response = ""
 
 """Override Chatbot.postprocess"""
-
 
 def postprocess(self, y):
     if y is None:
@@ -72,15 +74,24 @@ def parse_text(text):
 
 
 def predict(input, chatbot, max_length, top_p, temperature, history, past_key_values):
-    
+    global audio_text, response
+    print(f'in predict:{audio_text}')
+    if audio_text != "":
+        input = audio_text
     chatbot.append((parse_text(input), ""))
+    response = None
     for response, history, past_key_values in model.stream_chat(tokenizer, input, history, past_key_values=past_key_values,
                                                                 return_past_key_values=True,
                                                                 max_length=max_length, top_p=top_p,
                                                                 temperature=temperature):
         chatbot[-1] = (parse_text(input), parse_text(response))
-
+        # print(f'in predict:{response}')
         yield chatbot, history, past_key_values
+
+    # 这里已经生成完毕回答
+    print(response)
+    audio_text = ""
+    
 
 
 def reset_user_input():
@@ -91,6 +102,48 @@ def reset_state():
     return [], [], None
 
 
+from aip import AipSpeech
+
+APP_ID = '41498694'
+API_KEY = 'HG3sdTF1ildW78Cd8Oh77pcW'
+SECRET_KEY = 'n8LmPVvt3r65wgI3I4G8MXe60ojWQSWx'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+def baidu_audio_to_text(audio_path, format='wav', rate=16000):
+    """
+        音频文件需要先保存成wav文件，然后再把路径给到audio_path
+        这个函数返回由识别出的文字
+    """
+    with open(audio_path, 'rb') as fp:
+        data = fp.read()
+
+    result = client.asr(data, format, rate, {
+        'dev_pid': 1537,
+    })
+    return result['result'][0]
+
+# 保存音频的函数
+def save_audio(audio_input):
+
+    destination = "./saved_audio.wav"
+    shutil.copy(audio_input, destination)
+
+        # 使用pydub加载音频
+    audio = AudioSegment.from_wav(destination)
+    
+    # 转换为单通道
+    mono_audio = audio.set_channels(1).set_frame_rate(16000)
+    
+    # 保存为单通道音频文件
+    mono_audio.export('channel1.wav', format="wav")
+    global audio_text
+    audio_text = baidu_audio_to_text("./channel1.wav")
+    print(f'in save_audio:{audio_text}')
+
+def play_audio():
+    pass
+
 with gr.Blocks() as demo:
     gr.HTML("""<h1 align="center">ZhiPei Bot</h1>""")
 
@@ -98,10 +151,15 @@ with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=4):
             with gr.Column(scale=12):
-                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
-                    container=False)
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10, container=False)
+                submitBtn = gr.Button("Submit_text", variant="primary")
+                
             with gr.Column(min_width=32, scale=1):
-                submitBtn = gr.Button("Submit", variant="primary")
+                audio_input = gr.Audio(source="microphone", type="filepath", label="Your Voice", format='wav')
+                with gr.Column(min_width=16, scale=1):
+                    Submit_audio = gr.Button("Submit_audio", variant="primary")
+                with gr.Column(min_width=16, scale=1):
+                    audio_playBtn = gr.Button("Play response audio", variant="primary")
         with gr.Column(scale=1):
             emptyBtn = gr.Button("Clear History")
             max_length = gr.Slider(0, 32768, value=8192, step=1.0, label="Maximum length", interactive=True)
@@ -112,10 +170,20 @@ with gr.Blocks() as demo:
     past_key_values = gr.State(None)
 
     submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
-                    [chatbot, history, past_key_values], show_progress=True)
+                [chatbot, history, past_key_values], show_progress=True)
+    
+    # submitBtn.click(pr, [user_input], [], show_progress=True)
+
+    Submit_audio.click(save_audio, [audio_input],[], show_progress=True)
+    
     submitBtn.click(reset_user_input, [], [user_input])
 
+    Submit_audio.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
+                [chatbot, history, past_key_values], show_progress=True)
+
     emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
+
+    audio_playBtn.click(play_audio, [], [], show_progress=True)
 
 
 def main():
@@ -165,3 +233,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# 为什么第一次运行的时候是先运行predict再运行text_to_audio？
